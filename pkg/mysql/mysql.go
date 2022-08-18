cfb5d2102c8b947c52ab4753a8a35430d3850251
Jody-septiawan <jody.septiawan5@gmail.com>
2022-08-18 10:15:30 +0700

    BE

--- pkg/mysql/mysql.go
+++ pkg/mysql/mysql.go
@@ -33,7 +33,7 @@ func DatabaseInit() {
 	var DB_NAME = os.Getenv("DB_NAME")
 	var DB_PORT = os.Getenv("DB_PORT")
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai", DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT)
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT)
 	fmt.Println(dsn)
 	
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
